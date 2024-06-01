b6bddc3a5acf90f965cd43fc26c7875ac58f554f
Fahde2001 <132960725+Fahde2001@users.noreply.github.com>
2024-06-01 03:02:13 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Handler, Context, Callback } from 'aws-lambda';
-import { createServer, proxy } from 'aws-serverless-express';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
 
-let cachedServer;
-
-async function bootstrapServer() {
-  const expressApp = express();
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.init();
-  return createServer(expressApp);
+  await app.listen(3000, () => {
+    console.log('Nest application is listening on port 3000 over HTTP');
+  });
 }
 
-export const handler: Handler = async (event: any, context: Context, callback: Callback) => {
-  if (!cachedServer) {
-    cachedServer = await bootstrapServer();
-  }
-  return proxy(cachedServer, event, context, 'PROMISE').promise;
-};
+bootstrap();
